56ad4124686c9d21c33751982f4fbbf41e8b23da
developerpritam <pritambag426@gmail.com>
2024-09-04 23:18:52 +0530

    init

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let main_menu = document.getElementById("main_menu");
 mobile_icon.addEventListener("click", () => {
   main_menu.classList.toggle("hidden");
 });
+
+
+// sort section start
+// window.addEventListener("load", function(event) {
+//   document.querySelector('[data-dropdown-toggle="dropdown"]').click();
+// });
+// sort section end
